69c5d626dc8a557c329941be88472e1c2a095d89
余铁铮 <784250886@qq.com>
2019-03-30 14:14:24 +0800

    main function

---
+++ main.cpp
@@ -0,0 +1,51 @@
+//
+//  create by tiezheng yu on 2019-2-21
+//----- useage: ./main /home/icey/Desktop/project/KITTI/2011_09_26_drive_0005_sync/velodyne_points/data/0000000100.bin /home/icey/Desktop/project/KITTI/2011_09_26_drive_0005_sync/image_02/data/0000000100.png
+//
+
+
+#include "common_include.h"
+#include "data_loader.h"
+#include "mrf.h"
+
+int main(int argc, char **argv)
+{
+    string data_file = "/home/icey/Desktop/project/KITTI/2011_09_26_drive_0005_sync";
+    PointCloudLoader::Ptr point_cloud_loader (new PointCloudLoader(data_file));
+    ImageLoader::Ptr image_loader (new ImageLoader(data_file));
+    Calibration::Ptr calibration (new Calibration);
+    MRF::Ptr mrf (new MRF);
+
+    // pcl::visualization::PCLVisualizer viewer("result");//pcl viewer
+
+    for(int i = 0; i < 2; i++)
+    {
+        boost::timer timer;
+        
+        point_cloud_loader->readKittiPclBinData(i);
+        image_loader->readKittiImage(i);
+        mrf->calibration_ = calibration;
+        mrf->raw_cloud_ = point_cloud_loader->raw_cloud_;
+        mrf->raw_image_ = image_loader->image_;
+        mrf->MRFProcess();
+
+        cout<<"total cost time: "<<timer.elapsed() <<endl;
+
+
+
+
+
+
+        // cv::imshow("image", mrf->raw_image_);
+        // cv::waitKey(0);
+        // cv::destroyWindow("image");
+        // viewer.addPointCloud(point_cloud_loader->raw_cloud_,to_string(i));
+        // viewer.setBackgroundColor(0,0,0);
+        // viewer.addCoordinateSystem();
+        // viewer.spin();
+        // viewer.removeCoordinateSystem();
+        // viewer.removeAllPointClouds(); 
+    }
+
+    return 0;
+}
